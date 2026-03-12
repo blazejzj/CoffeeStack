88da54aa942cc6f8e95e595907b24e074d8c7e55
blazejzj <opexrrator@gmail.com>
2026-03-12 20:36:21 +0100

    feat: initial commit

---
+++ src/main/java/org/blazejzj/coffeestack/CoffeeStackApplication.java
@@ -0,0 +1,13 @@
+package org.blazejzj.coffeestack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeStackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/blazejzj/coffeestack/CoffeeStackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.blazejzj.coffeestack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
